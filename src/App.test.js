eb489d00924d6661f4f39af033f899198049a0d6
K B <kasia.gh.test1@gmail.com>
2021-02-11 18:56:02 +0100

    Added Layout

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders correctly', () => {
+  let wrapped = shallow(<App />);
+  expect(toJson(wrapped)).toMatchSnapshot();
 });
