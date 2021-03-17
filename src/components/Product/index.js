6188978c8c3fd7125fce2eab4389976b05482dc4
kasiaghtest1 <76445949+kasiaghtest1@users.noreply.github.com>
2021-03-17 16:58:19 +0100

    Added product details (#12)
    * added product details
    
    * added product details

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,11 +1,124 @@
 import styled from 'styled-components'
+import PageWrapper from '../PageWrapper'
+import media from '../../utils/media'
 
-const Details = styled.div``
+const product = { 
+  id: "1",
+  brand: "resibo",
+  name: "Cytrusowy Peeling Enzymatyczny w Żelu",
+  volume: "120ml",
+  price: "89zł",
+  type: "homeSpa",
+  img: "https://www.topestetic.pl/img/zdjecia/strona-big/resibo-regenerating-body-scrub-lotus-effect-regenerujacy-peeling-do-ciala-300-g.jpg?1613602801",
+  description: "Skuteczne złuszczanie i intensywna regeneracja po jednym użyciu. Peeling sprawia, że nawet bardzo zmęczona i sucha skóra od razu staje się gładka, miękka, silnie nawilżona i ujędrniona. Zrogowaciały naskórek znika, a specjalne dobrane aktywne składniki natychmiast pobudzają skórę do wytwarzania nowych, zdrowych komórek. Stosować podczas kąpieli. Nabrać peeling w dłonie i masować całe ciało okrężnymi ruchami. Zacząć od stóp i kierować się stopniowo w górę. Masować zawsze w kierunku serca."
+}
+
+const Wrapper = styled(PageWrapper)`
+  flex-direction: column;
+  padding: 15px;
+`
+const ContainerDetails = styled.div`
+  flex-direction: row;
+  display: flex;
+  padding-top: 20px;
+
+  ${media.phone} {
+    flex-direction: column;
+  }
+`
+
+const ContentDetails = styled.div`
+  flex: 1;
+`
+
+const ContentImage = styled(ContentDetails)`
+  display: flex;
+  justify-content: center;
+
+  ${media.tablet} {
+    padding: 20px;
+  }
+`
+
+const Image = styled.img`
+  max-width: 100%;
+  height: auto;
+`
+
+const BrandName = styled.div`
+  font-family: sans-serif;
+  font-weight: 600;
+  font-size: 20px;
+  padding-bottom: 5px;
+`
+
+const ProductName = styled.div`
+  font-weight: 600;
+  padding-bottom: 15px;
+`
+
+const ProductDetails = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 15px 0px;
+  border-top: 1px solid lightgrey;
+`
+
+const DeliverContent = styled(ProductDetails)`
+  ${media.tablet} {
+    flex-direction: column;
+  }
+`
+
+const Price = styled.div`
+  font-weight: 600;
+`
+
+const Volume = styled(Price)`
+  flex: 1;
+`
+
+const DeliveryInfo = styled.div`
+  font-weight: 600;
+  color: lightgreen;
+  flex: 1;
+`
+const Delivery = styled.div``
+
+const ContainerDescription = styled.div``
+
+const ProductDescription = styled.div`
+  font-weight: 600;
+`
 
 const Product = () => (
-  <Details>
-    Szczogóły produktu!!!
-  </Details>
+  <Wrapper>
+    <ContainerDetails>
+      <ContentImage>
+        <Image src={product.img} alt='photo'/>
+      </ContentImage>
+
+      <ContentDetails>
+        <BrandName>{product.brand}</BrandName>
+        <ProductName>{product.name}</ProductName >
+
+        <ProductDetails>
+          <Volume>{product.volume}</Volume>
+          <Price>{product.price}</Price>
+        </ProductDetails>
+
+        <DeliverContent>
+          <DeliveryInfo>Produkt dostępny</DeliveryInfo>
+          <Delivery>Dostawa gratis od 99zł</Delivery>
+        </DeliverContent>
+      </ContentDetails>
+    </ContainerDetails>
+
+    <ContainerDescription> 
+      <ProductDescription>Opis Produktu</ProductDescription>
+      {product.description}
+    </ContainerDescription>
+  </Wrapper>
 )
 
 export default Product
