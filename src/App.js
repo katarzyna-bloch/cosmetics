6414be8c4feeb497fa6f08b094983894b62b53d8
Katarzyna Bloch <katarzynabloch221@gmail.com>
2021-10-15 13:31:06 +0200

    Changed basename for React Router to support GitHub actions

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Footer from './components/Footer'
 import Routes from './Routes'
 
 const App = () => (
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <Header />
     <Navigation />
     <Main>
